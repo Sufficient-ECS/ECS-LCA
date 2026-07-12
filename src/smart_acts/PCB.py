f67a4114019ea8b60e5ce56738e08967aa12037d
auwattiez <augustin.wattiez@uclouvain.be>
2026-07-12 17:19:21 +0200

    Add a smart activity for PCB

---
+++ src/smart_acts/PCB.py
@@ -0,0 +1,48 @@
+import lca_algebraic as agb
+import sympy
+from functools import lru_cache
+from src.utils.utils import find_activity, get_param, unit_trans
+import logging
+
+def PCB_smart_activity(activity, param_name, db):
+
+    data = activity["data"]
+
+    pcb_area = get_param(f"{param_name}_pcb_area", data.get("area", None))
+
+    copper_layer_thickness = get_param(f"{param_name}_copper_layer_thickness", data.get("copper_layer", {}).get("thickness", None))
+    copper_layer_density = get_param(f"{param_name}_copper_layer_density", data.get("copper_layer", {}).get("density", None))
+    copper_layer_number = get_param(f"{param_name}_copper_layer_number", data.get("copper_layer", {}).get("number", None))
+
+    unit_area_gold_fingers = get_param(f"{param_name}_connectors_gold_fingers_unit_area", data.get("connectors", {}).get("gold_fingers", {}).get("unit_area", None))
+    number_gold_fingers = get_param(f"{param_name}_connectors_gold_fingers_number", data.get("connectors", {}).get("gold_fingers", {}).get("number", None))
+    unit_area_circular_connectors = get_param(f"{param_name}_connectors_circular_connectors_unit_area", data.get("connectors", {}).get("circular_connectors", {}).get("unit_area", None))
+    number_circular_connectors = get_param(f"{param_name}_connectors_circular_connectors_number", data.get("connectors", {}).get("circular_connectors", {}).get("number", None))
+    
+    thickness = get_param(f"{param_name}_connectors_gold_thickness", data.get("connectors", {}).get("thickness", None))
+    
+    acts = get_acts()
+
+    #the total must give something in m³
+    a0 = (acts[0], pcb_area*copper_layer_thickness*copper_layer_density*copper_layer_number*8960*agb.unit_registry("kg/m³")) #à séparer pour les unités / The standard density of pure copper at room temperature is 8960 kg/m³
+    #a0 = (acts[0], pcb_area*copper_layer_density*copper_layer_number*8960*agb.unit_registry("kg/m³")) #à séparer pour les unités / The standard density of pure copper at room temperature is 8960 kg/m³
+    a1 = (acts[1], (unit_area_gold_fingers*number_gold_fingers + unit_area_circular_connectors*number_circular_connectors)*thickness*19320*agb.unit_registry("kg/m³")) #The density of pure gold is 19320 kg/m³
+    a2 = (acts[2], pcb_area)
+
+    print(a0)
+    print(a1)
+    print(a2)
+
+    return [a0, a1, a2]
+
+
+@lru_cache(maxsize=1)
+def get_acts():
+    from src import OS_database
+    # import can't be moved to the start of the file because of circular import
+    return (
+        agb.findTechAct("market for copper, cathode", "GLO"),
+        agb.findTechAct("market for gold", "GLO"),
+        find_activity("pcb_no_copper_no_gold", "GLO", custom_db=OS_database),
+        #agb.findTechAct("market for printed wiring board, for through-hole mounting, Pb containing surface", "GLO"),
+    )
--- src/smart_acts/__init__.py
+++ src/smart_acts/__init__.py
@@ -1,6 +1,9 @@
 from src.smart_acts.chip import chip_smart_activity
+from src.smart_acts.PCB import PCB_smart_activity
 
 def smart_activity(activity, param_name, db):
     if activity["type"] == "chip":
         return chip_smart_activity(activity, param_name, db)
+    if activity["type"] == "pcb":
+        return PCB_smart_activity(activity, param_name, db)
     raise Exception("Activity type not supported")
